5f75c1f994dfb534ee4fb4c2fcf877fa796fded3
Geoffrey Pitman <gpitman379@gmail.com>
2016-04-02 01:25:02 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
-
-// programming competition question:
-// recieve binary number input as string
-// output hex number as string
 #include <iostream> 
 #include <string>        
 #include <bitset>
-#include <sstream>
 
 using namespace std;
 
 int main ()
 {
   string inp;
-  stringstream outp;
-  while(cin >> inp)
-  {
-	 bitset<1028> baz (string(inp.c_str()));
-	 outp << hex << baz.to_ulong() << endl;
-	 outp >> inp;
-	 for(int i = 0; i < inp.length(); i++)
-	 {
-			if(inp[i] > 96 && inp[i] < 122)
-				inp[i] -= 32;
-	 }
-	 cout << inp << endl;
-  }
-
+  
+  cin >> inp
+  bitset<2056> bits(string(inp.c_str()));
+  cout << hex << bits.to_ulong() << endl;
+  
   return 0;
 }
