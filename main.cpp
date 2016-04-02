3789e74e65156d82f18e0490150d934f1ab9638e
Geoffrey Pitman <gpitman379@gmail.com>
2016-04-02 02:19:35 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//get input string of 1s and 0s and output corresponding hex string
 #include <iostream> 
 #include <string>        
 #include <bitset>
@@ -14,3 +15,45 @@ int main ()
   
   return 0;
 }
+/*
+programming competition problem:
+get input string of 1s and 0s and output corresponding hex string
+#include <iostream>
+#include <string>
+#include <bitset>
+#include <sstream>
+
+using namespace std;
+
+int main ()
+{
+  string inp;
+  stringstream outp;
+  while(getline(cin,inp))
+  {
+    // remove spaces
+    string temp;
+    for(int i = 0; i< inp.length(); i++)
+      if(inp[i] != ' ')
+        temp += inp[i];
+    inp = temp;
+
+    // map string to actual bit values
+    bitset<1028> baz (string(inp.c_str()));
+
+    // convert to hex
+    outp << hex << baz.to_ulong() << endl;
+
+    // convert to upper
+    outp >> inp;
+    for(int i = 0; i < inp.length(); i++)
+     if(inp[i] > 96 && inp[i] < 122)
+      inp[i] -= 32;
+
+    // output converted string
+    cout << inp << endl;
+  }
+
+  return 0;
+}
+*/
