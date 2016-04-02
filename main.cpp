1c560bdab3cbd042076d78e1040d420d80fcd037
Geoffrey Pitman <gpitman379@gmail.com>
2016-04-02 02:28:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,28 +29,29 @@ int main ()
 {
   string inp;
   stringstream outp;
+  
   while(getline(cin,inp))
   {
-    // remove spaces
+    // remove spaces from input string
     string temp;
     for(int i = 0; i< inp.length(); i++)
       if(inp[i] != ' ')
         temp += inp[i];
     inp = temp;
 
-    // map string to actual bit values
+    // map input string to binary values
     bitset<1028> baz (string(inp.c_str()));
 
     // convert to hex
     outp << hex << baz.to_ulong() << endl;
 
-    // convert to upper
+    // convert to upper hex chars
     outp >> inp;
     for(int i = 0; i < inp.length(); i++)
      if(inp[i] > 96 && inp[i] < 122)
       inp[i] -= 32;
 
-    // output converted string
+    // output converted hex string
     cout << inp << endl;
   }
 
